465622cb794b3e6247f3fcd19fcfe7bb16d11e9e
sean <770899447@qq.com>
2022-06-04 14:51:27 +0800

    就地归并

--- MergeSort/index.js
+++ MergeSort/index.js
@@ -7,14 +7,14 @@ function merge(a, b) {
       result.push(b[r]);
       r += 1;
     } else if (r >= b.length) {
-      result.push(a[l])
+      result.push(a[l]);
       l += 1;
     } else if (a[l] < b[r]) {
-      result.push(a[l])
-      l += 1
+      result.push(a[l]);
+      l += 1;
     } else if (a[l] > b[r]) {
-      result.push(b[r])
-      r += 1
+      result.push(b[r]);
+      r += 1;
     }
   }
   return result;
@@ -27,3 +27,38 @@ function sort(array) {
   const right = array.slice(mid);
   return merge(soft(left), soft(right));
 }
+
+// 就地操作
+function _sort(array) {
+  return inplaceSort(array, 0, array.length);
+}
+
+function inplaceSort(array, start, end) {
+  if (end - start <= 1) return array;
+  const mid = Math.floor((start + end) / 2);
+  inplaceSort(array, start, mid);
+  inplaceSort(array, mid, end);
+  _merge(array, start, mid, end);
+  return array;
+}
+
+function _merge(array, start, middle, end) {
+  let l = start;
+  let r = middle;
+  while (l < middle && r < end) {
+    let w = 0;
+    while (array[l] <= array[r] && l < middle) {
+      l += 1;
+    }
+    while (array[l] >= array[r] && r < end) {
+      r += 1;
+      w += 1;
+    }
+    let part = array.splice(r - w, w);
+    array.splice(l, 0, ...part);
+    l += w;
+    middle += w;
+  }
+  return array;
+}
+console.log(_sort([1, 4, 112, 4, 5, 108, 12, 1281, 2]));
