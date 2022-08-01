b573be4d7f02317450be2e8877b92e4549222cf7
sean <770899447@qq.com>
2022-08-02 00:03:14 +0800

    四数之和

--- ThreeSum/index.js
+++ ThreeSum/index.js
@@ -2,8 +2,8 @@ const threeSumBf = require('./bf.js');
 const threeSumPivot = require('./pivot.js');
 const threeSumMap = require('./map.js');
 
-const nums = [2, 7, 11, 15, 3];
-const target = 12;
-console.log(threeSumBf(target, nums));
-console.log(threeSumPivot(target, nums));
+const nums = [-1, 0, 1, 2, -1, -4];
+// const target = 12;
+// console.log(threeSumBf(0, nums));
+console.log(threeSumPivot(nums));
 // console.log(threeSumMap(target, nums));
--- ThreeSum/pivot.js
+++ ThreeSum/pivot.js
@@ -1,16 +1,31 @@
-function threeSumPivot(target, nums) {
-  if (nums.length < 3) return;
-  const arr = nums.sort((a, b) => a - b);
-  for (let i = 0; i < arr.length - 3; i++) {
-    const theOtherTwo = target - arr[i];
+function threeSumPivot(nums) {
+  let n = nums.length;
+  if (n < 3) return;
+  let res = [];
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < n; i++) {
+    if (nums[i] > 0) return res;
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
     let left = i + 1;
-    let right = arr.length - 1;
+    let right = n - 1;
     while (left < right) {
-      if (arr[left] + arr[right] === theOtherTwo) return [arr[i], arr[left], arr[right]];
-      if (arr[left] + arr[right] < theOtherTwo) left++;
-      if (arr[left] + arr[right] > theOtherTwo) right--;
+      if (nums[i] + nums[left] + nums[right] === 0) {
+        res.push([nums[i], nums[left], nums[right]]);
+        while (left < right && nums[left] === nums[left + 1]) {
+          left++;
+        }
+        while (left < right && nums[right] === nums[right - 1]) {
+          right--;
+        }
+        left++;
+      } else if (nums[i] + nums[left] + nums[right] > 0) {
+        right--;
+      } else {
+        left++;
+      }
     }
   }
+  return res;
 }
 
 module.exports = threeSumPivot;
