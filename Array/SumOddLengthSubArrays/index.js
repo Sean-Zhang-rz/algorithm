dc78214a99eb05bd951157400702615e7efe0f12
sean <770899447@qq.com>
2022-08-07 15:30:35 +0800

    wip

--- Array/SumOddLengthSubArrays/index.js
+++ Array/SumOddLengthSubArrays/index.js
@@ -1,16 +1,15 @@
-function sumOddLengthSubArrays(arr){
-  let res = 0
-  let prefixSum = [0]
+function sumOddLengthSubArrays(arr) {
+  let res = 0;
+  let prefixSum = [0];
   for (let i = 0; i < arr.length; i++) {
-    prefixSum[i + 1] = prefixSum[i] + arr[i]
+    prefixSum[i + 1] = prefixSum[i] + arr[i];
   }
-  console.log(prefixSum);
   for (let i = 0; i < arr.length; i++) {
     for (let j = 1; j + i <= arr.length; j += 2) {
-      res = res + (prefixSum[i + j] - prefixSum[i])
+      res = res + (prefixSum[i + j] - prefixSum[i]);
     }
   }
-  return res
+  return res;
 }
-const nums = [1, 4, 2, 5, 3]
+const nums = [1, 4, 2, 5, 3];
 console.log(sumOddLengthSubArrays(nums));
