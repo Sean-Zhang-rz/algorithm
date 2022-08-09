8febbbcea0b7ad98bdf2d0f9a9e980aa78178a07
seanMr <770899447@qq.com>
2022-08-09 17:49:49 +0800

    the next greater number

---
+++ StackAndQueue/TheNextGreaterNumber/index.js
@@ -0,0 +1,57 @@
+function findNextGreaterNumber (nums){
+  if (nums == null || nums.length < 2) return nums;
+  const stack = [];
+  const res = Array.from({ length: nums.length}).fill(-1)
+  for (let i = 0; i < nums.length; i++) {
+    while (stack.length && nums[stack[stack.length - 1]] < nums[i]) {
+      const index = stack.pop();
+      res[index] = nums[i]
+    }
+    stack.push(i);
+  }
+  return res;
+}
+function findNextSmallerNumber (nums){
+  if (nums == null || nums.length < 2) return nums;
+  const stack = [];
+  const res = Array.from({ length: nums.length}).fill(-1)
+  for (let i = 0; i < nums.length; i++) {
+    while (stack.length && nums[stack[stack.length - 1]] > nums[i]) {
+      const index = stack.pop();
+      res[index] = nums[i]
+    }
+    stack.push(i);
+  }
+  return res;
+}
+function findLastGreaterNumber (nums){
+  if (nums == null || nums.length < 2) return nums;
+  const stack = [];
+  const res = Array.from({ length: nums.length}).fill(-1)
+  for (let i = nums.length - 1; i >0; i--) {
+    while (stack.length && nums[stack[stack.length - 1]] < nums[i]) {
+      const index = stack.pop();
+      res[index] = nums[i]
+    }
+    stack.push(i);
+  }
+  return res;
+}
+function findLastSmallerNumber (nums){
+  if (nums == null || nums.length < 2) return nums;
+  const stack = [];
+  const res = Array.from({ length: nums.length}).fill(-1)
+  for (let i = nums.length - 1; i > 0; i--) {
+    while (stack.length && nums[stack[stack.length - 1]] > nums[i]) {
+      const index = stack.pop();
+      res[index] = nums[i]
+    }
+    stack.push(i);
+  }
+  return res;
+}
+
+console.log(findNextGreaterNumber([2,4,3,5,1,3]));
+console.log(findNextSmallerNumber([2,4,3,5,1,3]));
+console.log(findLastGreaterNumber([2,4,3,5,1,3]));
+console.log(findLastSmallerNumber([2,4,3,5,1,3]));
