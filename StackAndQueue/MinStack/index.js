e928f56abf4dd453a8bfd6e9e4d845f4f19fa15d
sean <770899447@qq.com>
2022-08-13 23:22:46 +0800

    复习栈与队列

--- StackAndQueue/MinStack/index.js
+++ StackAndQueue/MinStack/index.js
@@ -1,22 +1,22 @@
 var MinStack = function () {
-  this.stack = []
-  this.minStack = [Infinity]
+  this.stack = [];
+  this.minStack = [Infinity];
 };
 
 MinStack.prototype.push = function (val) {
-  this.stack.push(val)
-  this.minStack.push(Math.min(this.minStack[this.minStack.length - 1], val))
+  this.stack.push(val);
+  this.minStack.push(Math.min(this.minStack[this.minStack.length - 1], val));
 };
 
 MinStack.prototype.pop = function () {
-  this.stack.pop()
-  this.minStack.pop()
+  this.stack.pop();
+  this.minStack.pop();
 };
 
 MinStack.prototype.top = function () {
-  return this.stack[this.stack.length - 1]
+  return this.stack[this.stack.length - 1];
 };
 
 MinStack.prototype.getMin = function () {
-  return this.minStack[this.minStack.length - 1]
+  return this.minStack[this.minStack.length - 1];
 };
