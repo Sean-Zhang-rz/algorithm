9a4c211aa644b94800215add034b138e5428cdde
seanMr <770899447@qq.com>
2022-08-16 20:01:36 +0800

    删除重复元素

---
+++ LinkedList/DeleteDuplicates2/index.js
@@ -0,0 +1,16 @@
+const LinkedList = require("../AchieveLinkedList");
+
+function deleteDuplicates2 (head) {
+  let dummyNode = new LinkedList(-1)
+  dummyNode.next = head
+  let node = dummyNode
+  while (node && node.next && node.next.next) {
+    if (node.next.val === node.next.next.val) {
+      let value = node.next.val;
+      while (node?.next && node.next.val === value) node.next = node.next.next
+    } else {
+      node = node.next
+    }
+  }
+  return dummyNode.next 
+}
