aaf3bbeac8d673cfd56b3d85ebb23e992cf0b6f5
sean <770899447@qq.com>
2022-08-25 21:44:27 +0800

    优化平衡二叉树

--- Tree/IsBalanced/index.js
+++ Tree/IsBalanced/index.js
@@ -1,3 +1,4 @@
+// O(nlogn) - O(n^2)
 function isBalanced(root) {
   if (!root) return true;
   if (Math.abs(getDepth(root.left) - getDepth(root.right)) > 1) return false;
@@ -5,4 +6,16 @@ function isBalanced(root) {
 }
 function getDepth(root) {
   return !root ? 0 : Math.max(getDepth(root.left), getDepth(root.right)) + 1;
+}
+
+// O(n)
+function isBalanced(root) {
+  return getHeight(root) !== -1;
+}
+function getHeight(root) {
+  if (!root) return 0;
+  let left = getHeight(root.left);
+  let right = getHeight(root.right);
+  if (left === -1 || right === -1 || Math.abs(left - right) > 1) return -1;
+  return Math.max(left, right) + 1;
 }
