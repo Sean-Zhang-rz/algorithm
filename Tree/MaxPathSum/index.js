f38670187e709ab8808494cc7720a342d1d8922a
seanMr <770899447@qq.com>
2022-08-26 14:45:16 +0800

    最大路径和

---
+++ Tree/MaxPathSum/index.js
@@ -0,0 +1,19 @@
+let max = -Infinity
+
+function maxPathSum (root) {
+  if (!root) return 0
+  divideAndCombine(root)
+  return max
+}
+
+function divideAndCombine(root){
+  if (!root) return -Infinity
+  let left = divideAndCombine(root.left)
+  let right = divideAndCombine(root.right)
+  const maxRes = Math.max(left, right)
+
+  let pathSum = Math.max(maxRes, 0) + root.val
+  let value = Math.max(pathSum, left + right + root.val)
+  max = Math.max(Math.max(value, pathSum), max)
+  return pathSum
+}
