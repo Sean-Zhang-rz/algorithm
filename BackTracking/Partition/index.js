fa277211b06eb19d490246cd8eaae90fc70af815
seanMr <770899447@qq.com>
2022-09-01 17:46:15 +0800

    分割回文淄川

---
+++ BackTracking/Partition/index.js
@@ -0,0 +1,21 @@
+function partition(s){
+  const result = []
+  if (!s) return result
+  const list = []
+  helper(result, list, s, 0)
+  return result
+}
+function helper(result, list, s, pos) {
+  if (s.length === pos) {
+    result.push([...list])
+    return
+  }
+  for(let i = pos; i < s.length; i++) {
+    const subStr = s.substring(pos, i + 1)
+    if (!isPalindrome(subStr)) continue
+    list.push(subStr)
+    helper(result, list, s, i + 1)
+    list.pop()
+  }
+}
+function isPalindrome(s){}
