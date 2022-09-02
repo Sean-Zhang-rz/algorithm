f500a23e756a69f8a43bd3e26a9ce3a47a886bba
seanMr <770899447@qq.com>
2022-09-02 21:06:29 +0800

    单词拆分2

---
+++ BackTracking/WordBreak2/index.js
@@ -0,0 +1,24 @@
+function wordBreak(s, wordDict) {
+  if (!s || !wordDict || !wordDict.length) return [];
+  const result = [];
+  const set = new Set();
+  for (let item of wordDict) {
+    set.add(item);
+  }
+  const list = [];
+  helper(result, list, s, set, 0);
+  return result;
+}
+function helper(result, list, s, set, pos) {
+  if (pos === s.length) {
+    result.push(list.join(' '));
+    return;
+  }
+  for (let i = pos; i < s.length; i++) {
+    const subStr = s.substring(pos, i + 1);
+    if (set.has(subStr)) continue;
+    list.push(subStr);
+    helper(result, list, s, set, i + 1);
+    list.pop();
+  }
+}
