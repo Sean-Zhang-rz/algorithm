a856186cb74c3f28c3d9b3a2bb2e829dbacbd236
seanMr <770899447@qq.com>
2022-09-05 19:58:28 +0800

    省份的数量

---
+++ Graph/findCircleNum/index.js
@@ -0,0 +1,19 @@
+function findCircleNumber(isConnectd) {
+  let count = 0
+  const length = isConnectd.length
+  const marked = new Array(length)
+  for (let i = 0; i < length; i++) {
+    if (!marked) {
+      dfs(isConnectd, i, marked)
+      count++
+    }
+  }
+  return count
+}
+function dfs(adj, no, marked) {
+  marked[no] = true
+  const values = adj[no]
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] === 1 && !marked[i]) dfs(adj, i, marked)
+  }
+}
