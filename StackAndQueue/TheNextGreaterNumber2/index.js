29b2257b46ca6bf7533a8768f3b236e92fe49985
seanMr <770899447@qq.com>
2022-09-13 16:59:10 +0800

    /

--- StackAndQueue/TheNextGreaterNumber2/index.js
+++ StackAndQueue/TheNextGreaterNumber2/index.js
@@ -3,7 +3,7 @@ var nextGreaterElements = function (nums) {
   const len = nums.length;
   if (len < 2) return [-1];
   const stack = [];
-  const res = Array.from({ length: len }).fill(-1);
+  const res = new Array(len).fill(-1);
   for (let i = 0; i < len * 2; i++) {
     while (stack.length && nums[stack[stack.length - 1]] < nums[i % len]) {
       res[stack.pop()] = nums[i % len];
