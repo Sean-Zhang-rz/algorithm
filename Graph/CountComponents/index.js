1b87c7916d72f3e0156e307b5594994502052145
seanMr <770899447@qq.com>
2022-09-16 16:38:44 +0800

    无向图里的连通分量(邻接表)

--- Graph/CountComponents/index.js
+++ Graph/CountComponents/index.js
@@ -1,26 +1,25 @@
-function countComponents(n, edges) {
+function countComponents(n, edges){
   let count = 0;
-  const adj = new Map();
+  const adj = new Map()
   for (let i = 0; i < n; i++) {
-    adj.set(i, []);
+    adj.set(i, [])
   }
   for (let i = 0; i < edges.length; i++) {
     adj.get(edges[i][0]).push(edges[i][1]);
     adj.get(edges[i][1]).push(edges[i][0]);
   }
-  const marked = [];
+  const visited = []
   for (let i = 0; i < n; i++) {
-    if (!marked[i]) {
-      dfs(adj, i, marked);
-      count++;
+    if (!visited[i]) {
+      dfs(adj, i, visited)
+      count++
     }
   }
-  return count;
+  return count
 }
-
-function dfs(adj, no, marked) {
-  marked[no] = true;
-  for (let adjNodeNo in adj) {
-    if (!marked[no]) dfs(adj, adjNodeNo, marked);
+function dfs(adj, no, visited) {
+  visited[no] = true
+  for (let adjNodeNo in adj.get(no)) {
+    if (!visited[adjNodeNo]) dfs(adj, adjNodeNo, visited)
   }
 }
