805b3be81e98c2ded369bdebdad9c6ce7783f683
seanMr <770899447@qq.com>
2022-09-20 19:51:34 +0800

    存在重复元素2

---
+++ Sort/ContainNearByDuplicates/index.js
@@ -0,0 +1,9 @@
+function containNearByDuplicates(nums, k) {
+  if (!nums || !nums.length) return false
+  const map = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i]) && map.get(nums[i]) - i <= k) return true
+    map.set(nums[i], i)
+  }
+  return false
+}
