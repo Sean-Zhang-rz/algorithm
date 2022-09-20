22494045bd58d1db084fd35600b55ddf847ee918
seanMr <770899447@qq.com>
2022-09-20 19:52:09 +0800

    /

--- Sort/ContainNearByDuplicates/index.js
+++ Sort/ContainNearByDuplicates/index.js
@@ -2,7 +2,7 @@ function containNearByDuplicates(nums, k) {
   if (!nums || !nums.length) return false
   const map = new Map()
   for (let i = 0; i < nums.length; i++) {
-    if (map.has(nums[i]) && map.get(nums[i]) - i <= k) return true
+    if (map.has(nums[i]) && Math.abs(map.get(nums[i]) - i) <= k) return true
     map.set(nums[i], i)
   }
   return false
