18eb13f8a7d0488b8b363201fa58cd1208c9172c
seanMr <770899447@qq.com>
2022-10-06 09:51:44 +0800

    小于2的最长字符

---
+++ Pointer/LengthOfLongestSubstringTwoDistinct/index.js
@@ -0,0 +1,26 @@
+function lengthOfLongestSubstringTwoDistinct(s) {
+  if (!s) return 0;
+  const map = new Map();
+  let j = 0;
+  let n = s.length;
+  let result = 0;
+  for (let i = 0; i < n; i++) {
+    while (j < n) {
+      const ch = s[j];
+      if (map.size < 2 || map.has(ch)) {
+        map.set(ch, j);
+        result = Math.max(result, j - i + 1);
+        j++;
+      } else {
+        // map.size === 2
+        break;
+      }
+    }
+    let midIndex = n - 1;
+    for (let index of map.values()) {
+      midIndex = Math.min(midIndex, index);
+    }
+    map.delete(s[midIndex]);
+    i = midIndex;
+  }
+}
