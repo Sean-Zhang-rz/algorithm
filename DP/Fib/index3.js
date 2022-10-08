5f38ddba94ef74c26a32a836d8c6bb372e1dc599
seanMr <770899447@qq.com>
2022-10-08 21:15:04 +0800

    滚动数组降低空间复杂度

---
+++ DP/Fib/index3.js
@@ -0,0 +1,10 @@
+function fib(n) {
+  if (n === 0 || n === 1) return n;
+  const memo = [];
+  memo[0] = 0;
+  memo[1] = 1;
+  for (let i = 0; i <= n; i++) {
+    memo[i % 2] = memo[(i - 1) % 2] + memo[(i - 2) % 2];
+  }
+  return memo[n % 2];
+}
