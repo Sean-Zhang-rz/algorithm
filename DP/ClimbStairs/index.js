4b4aa9076136031c3e6d11a521ba401df3db638b
seanMr <770899447@qq.com>
2022-10-08 21:48:45 +0800

    爬楼梯

--- DP/ClimbStairs/index.js
+++ DP/ClimbStairs/index.js
@@ -1,7 +1,7 @@
-function fib(n) {
+function climbStairs(n) {
   if (n === 0 || n === 1) return n;
   const memo = [];
-  memo[0] = 0;
+  memo[0] = 1;
   memo[1] = 1;
   for (let i = 2; i <= n; i++) {
     memo[i % 2] = memo[(i - 1) % 2] + memo[(i - 2) % 2];
--- DP/Fib/index.js
+++ DP/Fib/index.js
@@ -3,7 +3,7 @@ function fib(n) {
   const memo = [];
   memo[0] = 0;
   memo[1] = 1;
-  for (let i = 0; i <= n; i++) {
+  for (let i = 2; i <= n; i++) {
     memo[i] = memo[i - 1] + memo[i - 2];
   }
   return memo[n];
