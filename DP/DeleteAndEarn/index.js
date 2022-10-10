690e8afbfa29ed2b19580abdf2f86804d6c8586d
seanMr <770899447@qq.com>
2022-10-10 19:20:12 +0800

    /

--- DP/DeleteAndEarn/index.js
+++ DP/DeleteAndEarn/index.js
@@ -1,7 +1,7 @@
 function deleteAndEarn (nums) {
   if (!nums || !nums.length) return 0
   let max = 0
-    for (let num of nums) {
+  for (let num of nums) {
     max = Math.max(max, num)
   }
   const values = new Array(max + 1).fill(0)
@@ -10,7 +10,7 @@ function deleteAndEarn (nums) {
   }
   const memo = []
   memo[0] = values[0]
-  memo[1] = Math.max(values[0], values[1])
+  memo[1] = values[1]
   for (let i = 2; i < values.length; i++) {
     memo[i % 2] = Math.max(memo[(i - 1) % 2], memo[(i - 2) % 2] + values[i])
   }
